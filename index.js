769540c1a1521b648a97016b562240def32a12c9
Dr. Jochen Manns <dev-2@psimarron.net>
2026-01-02 11:14:01 +0100

    Readout guide.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-async function fetch(endpoint) {
+async function fetch(endpoint, data) {
   return new Promise((s) => {
     try {
       var req = new XMLHttpRequest();
@@ -13,8 +13,15 @@ async function fetch(endpoint) {
         }
       };
 
-      req.open("GET", `http://cardserver:81/api/${endpoint}`);
-      req.send();
+      req.open(data ? "POST" : "GET", `http://cardserver:81/api/${endpoint}`);
+
+      if (data) {
+        req.setRequestHeader("Content-Type", "application/json;chartset=utf-8");
+
+        req.send(JSON.stringify(data));
+      } else {
+        req.send();
+      }
     } catch {
       s(null);
     }
@@ -25,7 +32,7 @@ function failure(msg) {
   document.querySelector("pre").innerText = msg;
 }
 
-async function startup() {
+async function refresh() {
   const profiles = await fetch("profile/profiles");
   const profile = profiles && profiles[0];
 
@@ -40,5 +47,49 @@ async function startup() {
     l.localeCompare(r, undefined, { sensitivity: "accent" })
   );
 
-  document.querySelector("pre").innerText = JSON.stringify(sources, null, 2);
+  const query = {
+    contentPattern: "",
+    pageIndex: 0,
+    pageSize: 100,
+    profileName: profile.name,
+    source: "",
+    sourceEncryption: "All",
+    sourceType: "All",
+    startISO: "",
+    titlePattern: "",
+  };
+
+  const guide = await fetch("guide/query", query);
+
+  if (!guide || !guide.length) return failure("Keine Programmzeitschrift");
+
+  let map = sources.reduce((m, n) => ((m[n] = []), m), {});
+
+  for (const entry of guide) {
+    const source = map[entry.station];
+
+    if (source) source.push(entry);
+  }
+
+  map = Object.keys(map).reduce((m, n) => {
+    const split = n.indexOf("[");
+    const name = (n < 0 ? n : n.substring(0, split)).trim();
+
+    m[name] = map[n];
+
+    return m;
+  }, {});
+
+  const start = new Date(
+    Object.keys(map)
+      .flatMap((n) => map[n])
+      .map((e) => e.startTimeISO)
+      .sort((l, r) => l.localeCompare(r))[0]
+  );
+
+  document.querySelector("pre").innerText = start;
+}
+
+async function startup() {
+  refresh();
 }
